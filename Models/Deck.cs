a108cb533c6549dbc2b7139d194d1c23c9ad8308
ria-narai <ria.narai@student.uts.edu.au>
2024-10-13 17:58:25 +1100

    Initial commit for deck library feature

---
+++ Models/Deck.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+
+namespace PokeMemo.Models
+{
+    public partial class Deck : ObservableObject
+    {
+        private static int _nextId = 1;
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public List<Card> Cards { get; set; }
+
+        [ObservableProperty]
+        private string _backgroundColour;
+
+        [ObservableProperty]
+        private string _foregroundColour;
+
+        [ObservableProperty]
+        private string _borderColour;
+
+        public Deck(string name, string category, string backgroundColour, string foregroundColour, string borderColour)
+        {
+            Id = _nextId++;
+            Name = name;
+            Category = category;
+            Cards = new List<Card>();
+            _backgroundColour = backgroundColour;
+            _foregroundColour = foregroundColour;
+            _borderColour = borderColour;
+        }
+    }
+}
