46c578d9100a6815c73c4589632df44bf7630a66
ria-narai <ria.narai@student.uts.edu.au>
2024-10-18 02:46:09 +1100

    Implemented preliminary navigation - views and VMs for creating a deck, modifying a deck, quiz, and quiz results - and button navigation between deck library and create a deck as well as the quiz

---
+++ ViewModels/ModifyDeckViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace PokeMemo.ViewModels
+{
+    public partial class ModifyDeckViewModel : ViewModelBase
+    {
+        public ICommand NavigateToDeckLibraryViewCommand { get; }
+
+        public ModifyDeckViewModel()
+        {
+            NavigateToDeckLibraryViewCommand = new RelayCommand(o => NavigateToDeckLibraryView());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void NavigateToDeckLibraryView()
+        {
+            var mainWindowViewModel = (Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow.DataContext as MainWindowViewModel;
+            mainWindowViewModel?.NavigateToDeckLibraryViewCommand.Execute(null);
+        }
+    }
+}
