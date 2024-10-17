16ea55cbac10461152b5c8714851b0f9dd2f2018
Ramon Tovar <rtovar.develops@gmail.com>
2024-10-17 23:04:55 +1100

    Initial prototype of the window to create a new card. It's completely static and has no bindings.

---
+++ Views/CreateCardWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PokeMemo.Views;
+
+public partial class CreateCardWindow : Window
+{
+    public CreateCardWindow()
+    {
+        InitializeComponent();
+    }
+}
