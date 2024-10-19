32772c3a3485ff891a898c458df9618cf301cbfb
Ramon Tovar <rtovar.develops@gmail.com>
2024-10-20 09:57:18 +1100

    Added some boilerplate commands, fields and navigation from CreateCardView to PreviewDeckView.

--- ViewModels/CreateCardViewModel.cs
+++ ViewModels/CreateCardViewModel.cs
@@ -1,6 +1,33 @@
-namespace PokeMemo.ViewModels;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 
-public class CreateCardViewModel
+namespace PokeMemo.ViewModels
 {
+    public class CreateCardViewModel : ViewModelBase
+    {
+        /* Used to create a new card and display contents in the card preview */
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
     
+        public ICommand NavigateToPreviewDeckViewCommand { get; }
+        public ICommand CreateAndSaveNewCardCommand { get; }
+
+        public CreateCardViewModel()
+        {
+            NavigateToPreviewDeckViewCommand = new RelayCommand(o => NavigateToPreviewDeckView());
+            CreateAndSaveNewCardCommand = new RelayCommand(o => CreateAndSaveNewCard());
+        }
+
+        private void NavigateToPreviewDeckView()
+        {
+            var mainWindowViewModel = (Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow?.DataContext as MainWindowViewModel;
+            mainWindowViewModel?.NavigateToPreviewDeckViewCommand.Execute(null);
+        }
+
+        private void CreateAndSaveNewCard()
+        {
+            
+        }
+    }
 }
